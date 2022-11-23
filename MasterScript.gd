cdc07eb2d8a1be9e9914fb2d417dd545fe785159
Madelinya <madelinyaaa@gmail.com>
2022-11-23 16:18:55 +0100

    Data update #1

--- MasterScript.gd
+++ MasterScript.gd
@@ -15,6 +15,8 @@ var scene_order:Array[PackedScene]
 var right_button_down:bool = false
 var left_button_down:bool = false
 
+@onready var regex_searcher = RegEx.new()
+
 @onready var last_probed_detector = $Building/Detectors/f_right/f_right_probe
 @onready var camera_pivot = $CameraPivot
 
@@ -59,31 +61,13 @@ func _physics_process(_delta):
 	pass
 
 func _character_probed(probed_area) -> void:
-	var detector_index = detectors.find(probed_area)
-	var current_target_index:int = (int(target) / int(90.0))%4
-	var negative_mod = -2*(signi(current_target_index) - 1*abs(signi(current_target_index)))
-	var corrected_index = current_target_index + negative_mod
+	var _is_detector = detectors.find(probed_area) >= 0
 	
-	var turn_direction = 1
+	if _is_detector:
+		set_target_on_probe(probed_area)
+	else:
+		print( get_node_name(probed_area) +"_title")
 	
-	rotation_index = corrected_index
-	
-	if detector_index < corrected_index || ((detector_index == 3)&&(corrected_index == 0)):
-		turn_direction = -1 
-	elif detector_index == corrected_index:
-		turn_direction = 1
-	
-	var load_at = get_node("Building/SpawnPoints/%d" % overflow_int(rotation_index, turn_direction*2,4))
-	var to_load = scene_order[overflow_int(current_room, turn_direction * 2, scene_order.size())]
-	load_in_scene(load_at, to_load)
-	
-	current_room = overflow_int(current_room, turn_direction, scene_order.size())
-	target += 90*turn_direction
-	
-	last_probed_detector = probed_area
-	
-	print(current_room)
-	print(expositions.find(scene_order[current_room]))
 
 func match_scene_to_target(delta) -> void:
 	#Matches all parameters to target and executes rotation and such
@@ -123,15 +107,6 @@ func load_in_scene(spawn_on:Node, scene_to_spawn:PackedScene) -> void:
 			x.queue_free()
 	var scn = scene_to_spawn.instantiate()
 	spawn_on.add_child(scn)
-	pass
-
-func overflow_int(rot:int,dir:int,size:int) -> int:
-	var dir_n = dir%size
-	var rot_out = rot + dir_n
-	if rot_out < -0.001:
-		rot_out += size
-	rot_out = rot_out % size
-	return rot_out
 
 func initialize_scene_order() -> void:
 		#shuffles expositions
@@ -147,10 +122,43 @@ func initial_instantiation() -> void:
 		load_in_scene(target_node,scene_order[overflow_int(current_room, i, scene_order.size())])
 	pass
 
+func set_target_on_probe(probed_area:Area3D):
+	var detector_index = detectors.find(probed_area)
+	var current_target_index:int = (int(target) / int(90.0))%4
+	var negative_mod = -2*(signi(current_target_index) - 1*abs(signi(current_target_index)))
+	var corrected_index = current_target_index + negative_mod
+	
+	var turn_direction = 1
+	
+	rotation_index = corrected_index
+	
+	if detector_index < corrected_index || ((detector_index == 3)&&(corrected_index == 0)):
+		turn_direction = -1 
+	elif detector_index == corrected_index:
+		turn_direction = 1
+	
+	var load_at = get_node("Building/SpawnPoints/%d" % overflow_int(rotation_index, turn_direction*2,4))
+	var to_load = scene_order[overflow_int(current_room, turn_direction * 2, scene_order.size())]
+	load_in_scene(load_at, to_load)
+	
+	current_room = overflow_int(current_room, turn_direction, scene_order.size())
+	target += 90*turn_direction
+	
+	last_probed_detector = probed_area
+
 func _right_button() -> void:
 	right_button_down = !right_button_down
 func _left_button() -> void:
 	left_button_down = !left_button_down
-
+func overflow_int(rot:int,dir:int,size:int) -> int:
+	var dir_n = dir%size
+	var rot_out = rot + dir_n
+	if rot_out < -0.001:
+		rot_out += size
+	rot_out = rot_out % size
+	return rot_out
 func _exit_dialog() -> void:
 	dialog_container.visible = false
+func get_node_name(node:Node) -> String:
+	regex_searcher.compile("^()\\w+")
+	return regex_searcher.search(str(node)).get_string()
